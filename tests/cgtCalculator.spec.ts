dc85cbd8821034d5102cbf8e17c7897f1571106a
James Houghton <jameshoughton0123@gmail.com>
2024-08-07 23:11:44 +0100

    Additional tests for happy path scenarios and assertion fix

--- tests/cgtCalculator.spec.ts
+++ tests/cgtCalculator.spec.ts
@@ -4,5 +4,5 @@ test('has title', async ({ page }) => {
   await page.goto('https://www.tax.service.gov.uk/calculate-your-capital-gains/resident/properties/disposal-date');
 
   // Confirm the h1 of the webpage.
-  await expect(page).toHaveTitle('Welcome to GOV.UK');
+  await expect(page).toHaveTitle('When did you sell or give away the property? - Calculate your Capital Gains Tax - GOV.UK');
 });
--- tests/vehicleTax.spec.ts
+++ tests/vehicleTax.spec.ts
@@ -15,3 +15,13 @@ test('Vehicle search, field validation', async ({ page }) => {
   await page.getByRole('button', { name: 'Continue' }).click();
   expect(page.getByText('Error:Provide the vehicle')).toBeVisible;
 });
+
+test('Vehicle search, valid registration', async ({ page }) => {
+  await page.goto('https://vehicleenquiry.service.gov.uk/');
+  // Confirm happy path of valid regisration vehicle.
+  await page.getByLabel('Registration number (number').fill('KR24 XUV');
+  await page.getByRole('button', { name: 'Continue' }).click();
+  await page.getByLabel('Yes').check();
+  await page.getByRole('button', { name: 'Continue' }).click();
+  await expect(page).toHaveURL('https://vehicleenquiry.service.gov.uk/VehicleFound?locale=en');
+});
