1c305fd3533ef5801d4a3a6ad6c583991627fc16
James Houghton <jameshoughton0123@gmail.com>
2024-08-07 21:06:56 +0100

    Additional test for empty field validation

--- tests/vehicleTax.spec.ts
+++ tests/vehicleTax.spec.ts
@@ -1,9 +1,17 @@
 import { test, expect } from '@playwright/test';
 import assert from 'assert';
 
-test('Vehicle search', async ({ page }) => {
+test('Vehicle search, invalid registration', async ({ page }) => {
   await page.goto('https://vehicleenquiry.service.gov.uk/');
   // Enter the registration of a vehicle.
   await page.getByLabel('Registration number (number').fill('ABC123');
   await page.getByRole('button', { name: 'Continue' }).click();
+});
+
+test('Vehicle search, field validation', async ({ page }) => {
+  await page.goto('https://vehicleenquiry.service.gov.uk/');
+  // Confirm empty field validation.
+  await page.getByLabel('Registration number (number').fill('');
+  await page.getByRole('button', { name: 'Continue' }).click();
+  expect(page.getByText('Error:Provide the vehicle')).toBeVisible;
 });
