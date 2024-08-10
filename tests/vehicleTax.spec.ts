23325f81daa5b29116a1c61291c5756ac1971b26
James Houghton <jameshoughton0123@gmail.com>
2024-08-10 11:08:02 +0100

    Adding before.each hook to test

--- tests/vehicleTax.spec.ts
+++ tests/vehicleTax.spec.ts
@@ -1,15 +1,16 @@
 import { test, expect } from '@playwright/test';
-import assert from 'assert';
+
+test.beforeEach(async ({ page }) => {
+  await page.goto('https://vehicleenquiry.service.gov.uk/');
+});
 
 test('Vehicle search, invalid registration', async ({ page }) => {
-  await page.goto('https://vehicleenquiry.service.gov.uk/');
   // Enter the registration of a vehicle.
   await page.getByLabel('Registration number (number').fill('ABC123');
   await page.getByRole('button', { name: 'Continue' }).click();
 });
 
 test('Vehicle search, field validation', async ({ page }) => {
-  await page.goto('https://vehicleenquiry.service.gov.uk/');
   // Confirm empty field validation.
   await page.getByLabel('Registration number (number').fill('');
   await page.getByRole('button', { name: 'Continue' }).click();
@@ -17,7 +18,6 @@ test('Vehicle search, field validation', async ({ page }) => {
 });
 
 test('Vehicle search, valid registration', async ({ page }) => {
-  await page.goto('https://vehicleenquiry.service.gov.uk/');
   // Confirm happy path of valid regisration vehicle.
   await page.getByLabel('Registration number (number').fill('KR24 XUV');
   await page.getByRole('button', { name: 'Continue' }).click();
@@ -25,3 +25,4 @@ test('Vehicle search, valid registration', async ({ page }) => {
   await page.getByRole('button', { name: 'Continue' }).click();
   await expect(page).toHaveURL('https://vehicleenquiry.service.gov.uk/VehicleFound?locale=en');
 });
+
