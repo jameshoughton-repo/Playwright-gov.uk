7355bceff321cb948e94b12a2076797d76e90a7b
James Houghton <jameshoughton0123@gmail.com>
2025-01-20 17:25:52 +0000

    Fixing flakey tests

--- tests/search.spec.ts
+++ tests/search.spec.ts
@@ -7,5 +7,5 @@ test('Homepage search', async ({ page }) => {
   await expect(page).toHaveTitle('Welcome to GOV.UK');
   await page.getByLabel('Search', { exact: true }).fill('Childcare voucher');
   await page.keyboard.press('Enter');
-  await expect(page).toHaveURL('https://www.gov.uk/search/all?keywords=Childcare+voucher&order=relevance');
+  await expect(page).toHaveURL('https://www.gov.uk/search/all?keywords=Childcare+voucher');
 });
