29171749d44a51352035fd9f5911d19e7fb6cabb
James Houghton <jameshoughton0123@gmail.com>
2025-02-02 17:28:54 +0000

    Updating folder structure for POM

--- tests/cgtCalculator.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('has title', async ({ page }) => {
-  await page.goto('https://www.tax.service.gov.uk/calculate-your-capital-gains/resident/properties/disposal-date');
-
-  // Confirm the h1 of the webpage.
-  await expect(page).toHaveTitle('When did you sell or give away the property? - Calculate your Capital Gains Tax - GOV.UK');
-});
