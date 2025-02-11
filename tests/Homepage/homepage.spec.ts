5b35cb1295adc77d1500951793fb16a26e965a5c
jameshoughton <james.houghton@15below.com>
2025-02-11 13:56:34 +0000

    Test updates

--- tests/Homepage/homepage.spec.ts
+++ tests/Homepage/homepage.spec.ts
@@ -1,8 +1,11 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
+test('Homepage assertions', async ({ page }) => {
   await page.goto('https://www.gov.uk/');
 
   // Confirm the title of the website.
   await expect(page).toHaveTitle('Welcome to GOV.UK');
+
+  //Ensure the search function is visible on the homepage.
+  await expect (page.locator('id=search-main-ad39e476')).toBeVisible();
 });
