05f8eb409f2407005b51f879dc30c9070089f379
James Houghton <43765601+jameshoughton-repo@users.noreply.github.com>
2025-02-13 10:23:44 +0000

    Update vehicleTax.spec.ts

--- tests/Vehicle Enquiry/vehicleTax.spec.ts
+++ tests/Vehicle Enquiry/vehicleTax.spec.ts
@@ -1,5 +1,8 @@
 import { test, expect } from '@playwright/test';
 
+const invalidReg = "ABC123"
+const validReg = "KR24 XUV"
+
 test.beforeEach(async ({ page }) => {
   await page.goto('https://vehicleenquiry.service.gov.uk/');
   await expect(page.getByLabel('Registration number (number')).toBeVisible;
@@ -7,7 +10,7 @@ test.beforeEach(async ({ page }) => {
 
 test('Vehicle search, invalid registration', async ({ page }) => {
   // Enter the registration of a vehicle.
-  await page.getByLabel('Registration number (number').fill('ABC123');
+  await page.getByLabel('Registration number (number').fill(invalidReg);
   await page.getByRole('button', { name: 'Continue' }).click();
   await expect(page.getByRole('heading', { name: 'Vehicle details could not be' })).toBeVisible
 });
@@ -21,7 +24,7 @@ test('Vehicle search, field validation', async ({ page }) => {
 
 test('Vehicle search, valid registration', async ({ page }) => {
   // Confirm happy path of valid regisration vehicle.
-  await page.getByLabel('Registration number (number').fill('KR24 XUV');
+  await page.getByLabel('Registration number (number').fill(validReg);
   await page.getByRole('button', { name: 'Continue' }).click();
   await page.getByLabel('Yes').check();
   await page.getByRole('button', { name: 'Continue' }).click();
