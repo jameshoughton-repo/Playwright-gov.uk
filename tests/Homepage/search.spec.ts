8d55e13e5922bc39532e0947221cdaf283278f6e
jameshoughton <james.houghton@15below.com>
2025-02-13 10:13:41 +0000

    Updates

--- tests/Homepage/search.spec.ts
+++ tests/Homepage/search.spec.ts
@@ -2,10 +2,30 @@ import { test, expect } from '@playwright/test';
  
 const searchTerm = 'Childcare voucher'
 
+
 test.beforeEach('Test setup', async ({ page }) => {
   await page.goto('https://www.gov.uk/');
   await expect(page).toHaveTitle('Welcome to GOV.UK');
-  await page.getByLabel('Search', { exact: true }).fill('Childcare voucher');
+});
+
+test('Valid keyword search', async ({ page }) => {
+  // Perform a search on the homepage.
+  await page.getByLabel('Search', { exact: true }).fill(searchTerm);
   await page.keyboard.press('Enter');
   await expect(page).toHaveURL('https://www.gov.uk/search/all?keywords=Childcare+voucher');
+});
+
+test('Empty keyword search', async ({ page }) => {
+  // Perform an empty search on the homepage.
+  await page.getByLabel('Search', { exact: true }).fill('');
+  await page.keyboard.press('Enter');
+  await expect(page).toHaveURL('https://www.gov.uk/search/all?keywords=');
+});
+
+test('Pagination check', async ({ page }) => {
+  // Ensure the search pagenation is visible on the page.
+  await page.getByLabel('Search', { exact: true }).fill(searchTerm);
+  await page.keyboard.press('Enter');
+  await page.getByLabel('Pagination').click();
+  await expect(page).toHaveURL('https://www.gov.uk/search/all?keywords=Childcare+voucher&page=2');
 });
