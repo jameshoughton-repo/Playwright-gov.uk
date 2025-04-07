8ae3b9d499dac3719dfee537ba236e0349a7203e
jameshoughton <james.houghton@15below.com>
2025-04-07 15:26:31 +0100

    Updates to newsPage spec to be more readable

--- tests/News and communications/newsPage.spec.ts
+++ tests/News and communications/newsPage.spec.ts
@@ -1,13 +1,19 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, Page } from '@playwright/test';
+
+const url = 'https://www.gov.uk/search/news-and-communications';
 
 test.beforeEach(async ({ page }) => {
-  await page.goto('https://www.gov.uk/search/news-and-communications');
+  await page.goto(url);
   await expect(page.getByRole('heading', { name: 'News and communications' })).toBeVisible();
 });
 
+async function topicFilterClick(page: Page) {
+  await page.getByRole('button', { name: 'Topic'}).click();
+}
+
 test('Filtering and categorisation', async ({ page }) => {
   // Confirm the filtering on the news results.
-  await page.getByRole('button', { name: 'Topic'}).click();
+  await topicFilterClick(page);
   await page.getByLabel('Topic', { exact: true}).selectOption('Money');
   await page.getByRole('button', { name: 'Remove filter Money' }).isVisible();
 });
